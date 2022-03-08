69ddcf3c74e4471e7378f88c3050d6d0b6defd9e
Esteban Di Pietro <dipietroesteban@gmail.com>
2022-03-08 09:48:26 -0300

    exercise solutions

--- src/components/book-row.exercise.js
+++ src/components/book-row.exercise.js
@@ -2,7 +2,7 @@
 import {jsx} from '@emotion/core'
 
 // We're going to turn the entire book row into a link to the book page
-// 🐨 import the Link component from react-router-dom
+import {Link} from 'react-router-dom'
 import * as mq from 'styles/media-queries'
 import * as colors from 'styles/colors'
 
@@ -24,8 +24,9 @@ function BookRow({book}) {
           🐨 Turn this div into a Link
           and add a to prop to make it direct to `/book/${book.id}`
       */}
-      <div
+      <Link
         aria-labelledby={id}
+        to={`/book/${book.id}`}
         css={{
           minHeight: 270,
           flexGrow: 2,
@@ -88,7 +89,7 @@ function BookRow({book}) {
             {book.synopsis.substring(0, 500)}...
           </small>
         </div>
-      </div>
+      </Link>
     </div>
   )
 }
