2a002fe3cc090412ca302ebaff318df8a737a5db
Esteban Di Pietro <dipietroesteban@gmail.com>
2022-03-09 09:51:50 -0300

    create auth context and useauth hook

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,2 +1,17 @@
 // 🐨 create and export a React context variable for the AuthContext
 // 💰 using React.createContext
+import React from 'react'
+
+const AuthContext = React.createContext()
+
+function useAuth() {
+  const context = React.useContext(AuthContext)
+
+  if (context === undefined) {
+    throw new Error(`useAuth must be used within a AuthContext provider`)
+  }
+
+  return context
+}
+
+export {AuthContext, useAuth}
