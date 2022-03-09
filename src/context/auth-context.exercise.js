d612680df002cff7f5232f170ae115f09dc61864
Esteban Di Pietro <dipietroesteban@gmail.com>
2022-03-09 10:05:44 -0300

    create auth provider component with all the user logic

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,6 +1,11 @@
-// 🐨 create and export a React context variable for the AuthContext
-// 💰 using React.createContext
-import React from 'react'
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
+
+import * as React from 'react'
+import * as auth from 'auth-provider'
+import {FullPageSpinner, FullPageErrorFallback} from '../components/lib'
+import {client} from '../utils/api-client'
+import {useAsync} from '../utils/hooks'
 
 const AuthContext = React.createContext()
 
@@ -14,4 +19,53 @@ function useAuth() {
   return context
 }
 
-export {AuthContext, useAuth}
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+function AuthProvider(props) {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />
+  }
+
+  if (isSuccess) {
+    const value = {user, login, register, logout}
+    return <AuthContext.Provider value={value} {...props} />
+  }
+}
+
+export {AuthProvider, useAuth}
